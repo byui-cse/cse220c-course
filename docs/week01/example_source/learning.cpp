f3b59976a2937c5f64c57e27cb4869bb15555dc7
Nate Phillips <knathanp@gmail.com>
2021-06-09 15:06:11 -0600

    tweaked wording on VS community

--- docs/week01/example_source/learning.cpp
+++ docs/week01/example_source/learning.cpp
@@ -6,11 +6,13 @@
 //  This work is licensed under a Creative Commons Attribution 4.0 International License.
 //
 
-// Libraries and directives for Windows-based machines necessary to compile this program
-#ifdef _WIN32 
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdarg>
-#include <ctime>
+// Libraries and directives for Windows/Linux-based machines necessary to compile this program
+#if (defined(_WIN32) || defined(__linux__))
+  #ifdef _WIN32
+    #define _CRT_SECURE_NO_WARNINGS
+  #endif /* _WIN32 */
+  #include <cstdarg>
+  #include <ctime>
 #endif
 
 
@@ -18,35 +20,35 @@
 
 using namespace std;
 
-int total(int count, ...){
-    
-    va_list theNumbers;
+int total(int count, ...) {
 
-    va_start (theNumbers, count);// Set up the list of numbers.
+  va_list theNumbers;
 
-    int sum = 0;
-    for (int i = 0; i < count; i++){//similar to using for..in with a range in Python
-        int anInt = va_arg(theNumbers, int);//Get the next int from the list of ints
-      sum += anInt;
-    }
-    va_end(theNumbers);// Clean up.
-    return sum;
+  va_start(theNumbers, count); // Set up the list of numbers.
+
+  int sum = 0;
+  for (int i = 0; i < count; i++) { //similar to using for..in with a range in Python
+    int anInt = va_arg(theNumbers, int); //Get the next int from the list of ints
+    sum += anInt;
+  }
+  va_end(theNumbers); // Clean up.
+  return sum;
 }
 
-int main(int argc, const char * argv[]) {
-    
-    // current date/time based on current system
-    time_t now = time(0);
-    
-    // convert now to string form
-    string currentDateAndTime = ctime(&now);
+int main(int argc, const char* argv[]) {
 
-    cout<<"It is now "<< currentDateAndTime<<endl;
+  // current date/time based on current system
+  time_t now = time(0);
+
+  // convert now to string form
+  string currentDateAndTime = ctime(&now);
+
+  cout << "It is now " << currentDateAndTime << endl;
 
 
-    int theTotal = total(10,1,2,3,4,5,6,7,8,9,10);
+  int theTotal = total(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    cout<<"The total is "<<theTotal<<endl;
-    return 0;
+  cout << "The total is " << theTotal << endl;
+  return 0;
 }
 
